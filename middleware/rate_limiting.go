f2db02bb55cbfac25d61c7dc13ceb74eb89335f1
Sadiq <riomahod@gmail.com>
2024-10-17 08:14:12 +0300

    refactor: replaced custom rate limit logic with go.uber.org/ratelimit for improved performance and simplicity
    - Removed manual rate limit management using sync.Mutex and visitor count map.
    - Integrated go.uber.org/ratelimit to handle rate limiting in a more efficient way.
    - Removed unnecessary go routines for resetting visitor counts.
    - Simplified logic by using LoadOrStore to handle IP-based rate limiting.
    - The new implementation provides better scalability and reduces complexity by relying on an optimized external library.

--- middleware/rate_limiting.go
+++ middleware/rate_limiting.go
@@ -2,18 +2,16 @@ package middleware
 
 import (
 	"sync"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/pageton/authify/config"
+	"go.uber.org/ratelimit"
 )
 
 var (
 	cfg       *config.Config
 	rateLimit int
-	interval  = time.Second
-	visitors  = make(map[string]int)
-	mu        sync.Mutex
+	rlMap     sync.Map
 )
 
 func Init() error {
@@ -29,24 +27,17 @@ func Init() error {
 func RateLimitMiddleware(c *fiber.Ctx) error {
 	ip := c.IP()
 
-	mu.Lock()
+	limiter, _ := rlMap.LoadOrStore(ip, ratelimit.New(rateLimit))
 
-	defer mu.Unlock()
+	rl := limiter.(ratelimit.Limiter)
 
-	visitors[ip]++
+	rl.Take()
 
-	if visitors[ip] > rateLimit {
-		return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{"error": "Too many requests. Please try again later."})
+	if _, ok := rlMap.Load(ip); !ok {
+		return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+			"error": "Too many requests. Please try again later.",
+		})
 	}
 
-	go resetVisitorCount(ip)
-
 	return c.Next()
-}
-
-func resetVisitorCount(ip string) {
-	time.Sleep(interval)
-	mu.Lock()
-	defer mu.Unlock()
-	visitors[ip]--
 }
